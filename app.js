c972a3bd8c669418760440aff68518f0ac9bddb8
MAntimil <antimilmartin158@gmail.com>
2025-07-24 15:08:59 -0400

    manejoarray

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-let contador = 1;
 const formulario = document.querySelector(".form-create");
-formulario.addEventListener("submit", (e)=>{
+let cont = 1;
+formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
-    contenedor.innerHTML +=` <div class="publicacion"div>
-    <p>id:${contador}</p>
-    <p>${descripcion}</p>
-    <button id="Editar" onclick="editar(this)">Editar</button>
-    <button id="Eliminar" onclick="eliminar(this)">Eliminar</button>
-    </div> `;
-    ++contador;
+    contenedor.innerHTML += `<div id="${cont}" class="publicacion">
+            <p>id:${cont} </p>
+            <p>${descripcion}</p>
+            <button id="editar" onclick="editar(this)">Editar</button>
+            <button id="eliminar" onclick="eliminar(this)">Eliminar</button>
+        </div>`;
+    cont++;
 });
 
 
-function editar(e){
+function editar(e) {
     const parrafo = e.parentElement.querySelector("p:nth-child(2)");
-    parrafo.contentEditable = true;
-    e.value = "guardar";
-    if(parrafo.contentEditable){
-        parrafo.contentEditable = false;
+    if (e.textContent == "Editar") {
+        parrafo.contentEditable = true;
+        e.textContent = "Guardar"
+        parrafo.focus();
     }
-    if(e.value == "guardar"){
-        e. value "Editar";
+    else {
+        e.textContent = "Editar"
+        parrafo.contentEditable = false;
     }
 }
 
-function eliminar(e){
-    const publicacion = e.parentElement;
-    publicacion.remove();
+function eliminar(e) {
+    e.parentElement.remove();
 }
